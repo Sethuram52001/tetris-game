fa0c29129ea6181bff6ad5d36828bd4ad73646f8
Sethuram.S.V <58566745+Sethuram52001@users.noreply.github.com>
2020-07-16 18:50:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tetris from './components/Tetris';
+
+export default class App extends React.Component {
+  
+state={
+
+}
+
+  render() {
+    return (
+      <div>
+        <Tetris></Tetris>
+      </div>
+    );
+  }
+}
